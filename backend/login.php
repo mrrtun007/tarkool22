7c08c7684cc68281054663e6e388a6d811e68c2a
mrrtun007 <68525887+mrrtun007@users.noreply.github.com>
2021-06-05 05:03:28 +0700

    first commit

---
+++ backend/login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>tarkool</title>
+</head>
+<body>
+<form name="form1" method="post" action="login_db.php">
+  Login<br>
+  <table border="1" style="width: 300px">
+    <tbody>
+      <tr>
+        <td> &nbsp;Username</td>
+        <td>
+          <input name="a_user" type="text" id="a_user">
+        </td>
+      </tr>
+      <tr>
+        <td> &nbsp;Password</td>
+        <td><input name="a_pass" type="password" id="a_pass">
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <br>
+  <input type="submit" name="Submit" value="Login">
+</form>
+</body>
+</html>
